1bc2c385c7777aefca5330d3375c52225a79bd5b
Arpit Tiwari <168163492+Arpittiwari2711@users.noreply.github.com>
2026-07-23 19:29:54 +0530

    Time: 11 ms (51.95%), Space: 12.4 MB (90.01%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def countAndSay(self, n):
+        if n == 1:
+            return "1"
+        prev = self.countAndSay(n-1)
+        result = ""
+        count = 1
+
+        for i in range(len(prev)):
+            if i + 1 < len(prev) and prev[i] == prev[i+1]:
+                count +=1
+            else:
+                result += str(count) + prev[i]
+                count =1
+        return result
