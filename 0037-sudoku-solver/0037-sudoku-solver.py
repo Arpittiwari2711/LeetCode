ed4ba76e41a2366165798bcbcc5ad5eef37f2df0
Arpit Tiwari <168163492+Arpittiwari2711@users.noreply.github.com>
2026-07-23 19:43:07 +0530

    Time: 2140 ms (58.15%), Space: 12.5 MB (57.93%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -0,0 +1,55 @@
+class Solution(object):
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None
+        """
+
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empty = []
+
+        # Initialize sets and empty cells
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    empty.append((r, c))
+                else:
+                    num = board[r][c]
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[(r // 3) * 3 + (c // 3)].add(num)
+
+        def solve(index):
+            # If all empty cells are filled
+            if index == len(empty):
+                return True
+
+            r, c = empty[index]
+            box = (r // 3) * 3 + (c // 3)
+
+            for num in "123456789":
+                if (
+                    num not in rows[r]
+                    and num not in cols[c]
+                    and num not in boxes[box]
+                ):
+                    # Place number
+                    board[r][c] = num
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[box].add(num)
+
+                    if solve(index + 1):
+                        return True
+
+                    # Backtrack
+                    board[r][c] = "."
+                    rows[r].remove(num)
+                    cols[c].remove(num)
+                    boxes[box].remove(num)
+
+            return False
+
+        solve(0)
